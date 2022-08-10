a5f39d12a024c2df3680112c731110c171e3afca
abhiram <abhiram8034@gmail.com>
2022-08-10 13:44:58 +0000

    single page application addes

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="notfound-container">
+    <img
+      alt="notfound"
+      className="notfound-img"
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+    />
+    <h1 className="notfound-heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
